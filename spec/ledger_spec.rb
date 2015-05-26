57ac039cc47f5df77cfd72a5ba56500714f25bdc
bibiana <bcristofol@gmail.com>
2015-05-26 10:38:52 +0100

    Refactored ledger spec

--- spec/ledger_spec.rb
+++ spec/ledger_spec.rb
@@ -3,36 +3,32 @@ require 'ledger'
 describe Ledger do
 
   let(:ledger) { Ledger.new }
+  let(:user1) { User.new('John') }
+  let(:user2) { User.new('Mary') }
+  let(:user3) { User.new('Supermarket') }
   
   it "should have a date" do
     expect(ledger.date).to eq DateTime.now.strftime('%Y/%m/%d')
   end
 
   it "should transfer money from one account to another one" do
-    user1 = User.new('John')
-    user2 = User.new('Mary')
-    ledger.transfer(10, 'John', 'Mary')
+    ledger.transfer(10, user1.name, user2.name)
     expect(user1.account.balance).to eq -10
     expect(user2.account.balance).to eq 10
   end
 
   it "should add a transaction" do
-    user1 = User.new('John')
-    user2 = User.new('Mary')
-    user3 = User.new('Supermarket')
-    ledger.add_transaction('2015/05/12', 10, 'John', 'Mary')
-    ledger.add_transaction('2015/05/12', 50, 'Mary', 'Supermarket')
-    ledger.add_transaction('2015/05/12', 20, 'Mary', 'John')
+    ledger.add_transaction('2015/05/12', 10, user1.name, user2.name)
+    ledger.add_transaction('2015/05/12', 50, user2.name, user3.name)
+    ledger.add_transaction('2015/05/12', 20, user2.name, user1.name)
     expect(user1.account.balance).to eq 10
     expect(user2.account.balance).to eq -60
     expect(user3.account.balance).to eq 50
   end
 
   it "should be able to provide a balance for a given date" do
-    user1 = User.new('John')
-    user2 = User.new('Mary')
-    ledger.add_transaction('2015/05/12', 10, 'John', 'Mary')
-    ledger.add_transaction('2015/05/15', 50, 'Mary', 'John')
+    ledger.add_transaction('2015/05/12', 10, user1.name, user2.name)
+    ledger.add_transaction('2015/05/15', 50, user2.name, user1.name)
     expect(ledger.lookup_balance('2015/05/12', 'John')).to eq -10
     expect(ledger.lookup_balance('2015/05/16', 'Mary')).to eq -40
   end
